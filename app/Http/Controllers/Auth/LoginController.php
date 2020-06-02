40fdf3c85c3f8cc3876eae6ab3e260203d6d9e30
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-06-02 11:50:23 +0600

    after login redirect solve

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/payment';
 
     /**
      * Create a new controller instance.
@@ -37,7 +38,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-
+    protected function authenticated(Request $request, $user)
+    {
+        return redirect()->route('payment');
+    }
     public function username(){
         return 'username';
     }
