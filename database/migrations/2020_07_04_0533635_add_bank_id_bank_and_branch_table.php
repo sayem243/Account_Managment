149cebf41d538b755aa55acf282f5178f8b7333b
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-07-07 18:41:33 +0600

    migration file update add bank id field file

--- database/migrations/2020_07_04_0533635_add_bank_id_bank_and_branch_table.php
+++ database/migrations/2020_07_04_0533635_add_bank_id_bank_and_branch_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddBankIdFieldBankAndBranchTable extends Migration
+class AddBankIdBankAndBranchTable extends Migration
 {
     /**
      * Run the migrations.
