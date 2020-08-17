a1adf40bb2bf56ecfe861e2b454377534a342060
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-08-17 16:37:37 +0600

    check registry list page and daily cash balance page done.

--- resources/views/check_registry/index.blade.php
+++ resources/views/check_registry/index.blade.php
@@ -75,7 +75,7 @@
          <div class="modal-content">
              <div class="modal-header" style="display: block">
                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                 <h4 class="modal-title" id="myModalLabel">Payment Details</h4>
+                 <h4 class="modal-title" id="myModalLabel">Check Registry Details</h4>
              </div>
 
              <div class="modal-body">
