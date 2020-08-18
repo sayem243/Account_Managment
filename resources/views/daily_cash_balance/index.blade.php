5521d6643d33e32a4c50eb0be17d1c4f47f0ea07
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-08-18 14:58:59 +0600

    button lable change

--- resources/views/daily_cash_balance/index.blade.php
+++ resources/views/daily_cash_balance/index.blade.php
@@ -255,7 +255,7 @@
                                 <div style="padding-right: 3px"
                                      class="col-sm-12 col-form-label btn-group btn-group-lg" align="right">
                                     <button style="margin-right: 0" name="closing_update" value="closing_update" type="submit" class="btn btn-info"><i
-                                                class="feather icon-save"></i> End of the close
+                                                class="feather icon-save"></i> End of the day
                                     </button>
                                 </div>
                             </div>
