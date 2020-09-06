064e47811513be677598a8a04b7b58acd92465d2
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-09-06 18:11:29 +0600

    client migration added and loan view, quick view and print design modify

---
+++ app/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    //
+
+    use SoftDeletes;
+}
