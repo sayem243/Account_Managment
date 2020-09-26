0c784db360a85ecbfab6eb753595bb40fb65ecee
rakeybulhasan <rakeybulhasan@gmail.com>
2020-09-26 19:03:37 +0600

    is old added into disbusred button

--- app/CashDailyBalanceSession.php
+++ app/CashDailyBalanceSession.php
@@ -3,11 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class CashDailyBalanceSession extends Model
 {
     public function company(){
 
         return $this->belongsTo('App\Company','company_id')->withTrashed();
+    }
+
+    public function getDailyOpeningClosingBalance($from_date,$to_date, $company_id){
+        $rows = DB::table('cash_daily_balance_sessions');
+        if ($company_id!=''){
+            $rows->where('company_id', $company_id);
+        }
+        $rows->whereBetween('created_at', [$from_date, $to_date]);
+        $rows->orderBy('id', 'ASC');
+        $result = $rows->get();
+        $returnData = array();
+
+        foreach ($result as $value){
+            $returnData[$value->company_id]=$value;
+        }
+        return $returnData;
     }
 }
--- app/CashTransaction.php
+++ app/CashTransaction.php
@@ -14,4 +14,33 @@ class CashTransaction extends Model
 
         return DB::getPDO()->lastInsertId();
     }
+
+
+    public function getDailyBalanceTransaction($date, $company_id){
+        $from_date = $date? $date.' 00:00:00': '';
+        $to_date = $date? $date.' 23:59:59': '';
+        $rows = DB::table('cash_transactions');
+        if ($company_id!=''){
+            $rows->where('company_id', $company_id);
+        }
+        $rows->whereIn('cash_transactions.transaction_via',[
+            'CHECK_OUT',
+            'INCOME_CASH',
+            'INCOME_CHECK_CASH',
+            'LOAN_CASH',
+            'LOAN_CHECK_CASH',
+            'HAND_SLIP_SETTLE',
+            'HAND_SLIP_TRANSFER',
+            'HAND_SLIP_CASH_RETURN',
+            'HAND_SLIP_ISSUE',
+            'VOUCHER',
+        ]);
+        $rows->whereBetween('created_at', [$from_date, $to_date]);
+        $result = $rows->get();
+        $returnArray = array();
+
+        foreach ($result as $cashTransaction){
+            $returnArray[$cashTransaction->company_id][$cashTransaction->transaction_type][]=$cashTransaction->amount;
+        }
+    }
 }
