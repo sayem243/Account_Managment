3ee3d1a3c100dfec6aa22d3177976bb604291b9d
MirFahim94 <fahim@rightbrainsolution.com>
2020-09-27 15:45:35 +0600

    permisionList

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -36,11 +36,13 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Permissions:</strong>
+                <ul>
                 @if(!empty($rolePermissions))
                     @foreach($rolePermissions as $v)
-                        <label class="label label-success">{{ $v->name }},</label>
+                            <li><label class="label label-success">{{ $v->name }},</label></li>
                     @endforeach
                 @endif
+                </ul>
             </div>
         </div>
     </div>
