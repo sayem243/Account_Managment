c3ec58938559aeb5f40a695ec461e69926d48962
rakeybulhasan <rakeybulhasan@gmail.com>
2020-10-03 10:35:09 +0600

    loan quick view amount in word only added

--- resources/views/loan_income/loan/loan_quick_view.blade.php
+++ resources/views/loan_income/loan/loan_quick_view.blade.php
@@ -248,7 +248,7 @@
                                     @php use App\CustomClass\NumberToWordConverter;
                                     $amount = NumberToWordConverter::convert($loan->amount);
                                     @endphp
-                                    {{$amount}}
+                                    {{$amount}} only.
                                     </span>
                             </label>
                         </div>
